767e7a405dfc4b554270006a81e70ce48868210d
Donovan Hill <hilld13@mytru.ca>
2021-02-20 08:27:54 -0800

    create activity for planet data and add button to main activity

--- app/src/main/java/com/example/comp_2160_project/MainActivity.java
+++ app/src/main/java/com/example/comp_2160_project/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.comp_2160_project;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void activityPlanetData(View view) {
+        Intent actPD = new Intent(this, PlanetData.class);
+        startActivity(actPD);
     }
 }
